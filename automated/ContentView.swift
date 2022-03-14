e673b0cb91735ebe24892bf9270eaf0389e4802e
Bekdaulet <4bekdaulet@gmail.com>
2022-03-14 11:08:47 +0600

    Initial Commit

---
+++ automated/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  automated
+//
+//  Created by Бекдаулет Касымов on 14.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ automated/automatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  automatedApp.swift
+//  automated
+//
+//  Created by Бекдаулет Касымов on 14.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct automatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
